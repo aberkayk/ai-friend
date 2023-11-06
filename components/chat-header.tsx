c3f14701ac271f83b41a08244f6a424e7e8ed62b
aberkayk <aberkayk@gmail.com>
2023-11-06 12:54:11 +0300

    Add chat header

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -1,6 +1,23 @@
 "use client";
 
 import { Companion, Message } from "@prisma/client";
+import { Button } from "@/components/ui/button";
+import {
+  ChevronLeft,
+  Edit,
+  MessageSquare,
+  MoreVertical,
+  Trash,
+} from "lucide-react";
+import { useRouter } from "next/navigation";
+import BotAvatar from "@/components/bot-avatar";
+import { useUser } from "@clerk/nextjs";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 interface ChatHeaderProps {
   companion: Companion & {
@@ -12,7 +29,53 @@ interface ChatHeaderProps {
 }
 
 const ChatHeader = ({ companion }: ChatHeaderProps) => {
-  return <div>ChatHeader</div>;
+  const router = useRouter();
+  const { user } = useUser();
+
+  return (
+    <div className="flex w-full justify-between items-center border-b border-primary/10 pb-4">
+      <div className="flex gap-x-2 items-center">
+        <Button onClick={() => router.back()} size="icon" variant="ghost">
+          <ChevronLeft className="h-6 w-6" />
+        </Button>
+        <BotAvatar src={companion.src} />
+        <div className="flex flex-col gap-y-1">
+          <div className="flex items-center gap-x-2">
+            <p>{companion.name}</p>
+            <div
+              className="
+            flex items-center text-xs text-muted-foreground"
+            >
+              <MessageSquare className="h-3 w-3 mr-1" />
+              {companion._count.messages}
+            </div>
+          </div>
+          <p className="text-xs text-muted-foreground">
+            Created by {companion.userName}
+          </p>
+        </div>
+      </div>
+      {user?.id === companion.userId && (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary" size="icon">
+              <MoreVertical />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem>
+              <Edit className="w-4 h-4 mr-2" />
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Trash className="w-4 h-4 mr-2" />
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )}
+    </div>
+  );
 };
 
 export default ChatHeader;
